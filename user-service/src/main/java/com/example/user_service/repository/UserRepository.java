5b457a4ba81ed5c8aeebd4f157ed37bf7b74b481
v4nhs <nguyenvietanhcv21072003@gmail.com>
2025-08-08 11:23:41 +0700

    big

--- user-service/src/main/java/com/example/user_service/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.user_service.repository;
-
-import com.example.user_service.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
